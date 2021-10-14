16b16ac0f2f5800c86ee4bb9303e6eb993350781
alaneh <aelizaldeh1900@alumno.ipn.mx>
2021-10-13 20:27:55 -0500

    Agregar lo que resta de administrador

---
+++ QUETZUAL (administrador)/JS/funcionModal2.js
@@ -0,0 +1,18 @@
+const openEls = document.querySelectorAll("[data-open]");
+const isVisible = "is-visible";
+
+for (const el of openEls) {
+    el.addEventListener("click", function() {
+        const modalId = this.dataset.open;
+        document.getElementById(modalId).classList.add(isVisible);
+    });
+}
+
+document.addEventListener("click", e => {
+    if (e.target == document.querySelector(".modal.is-visible")) {
+        document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    }
+    if (e.target == document.querySelector(".modal2.is-visible")) {
+        document.querySelector(".modal2.is-visible").classList.remove(isVisible);
+    }
+});
