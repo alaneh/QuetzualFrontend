3854eb6bf8c3f79366726a97e7ede61728d5d535
alaneh <aelizaldeh1900@alumno.ipn.mx>
2021-10-15 07:46:20 -0500

    Agregar las conexiones de administrador

---
+++ QUETZUAL (administrador)/JS/redirigir.js
@@ -0,0 +1,23 @@
+function cerrarSesion() {
+    location.href = '../index.html';
+}
+
+function enviarConsultarHistorico() {
+    location.href = './consultarHistorico.html';
+}
+
+function enviarAdminDoctores() {
+    location.href = './adminDoctores.html';
+}
+
+function enviarCuentaAdmin() {
+    location.href = './cuentaAdmin.html';
+}
+
+function enviarRankingAdmin() {
+    location.href = './rankingAdmin.html';
+}
+
+function enviarSesionAdmin() {
+    location.href = './sesionAdmin.html';
+}
